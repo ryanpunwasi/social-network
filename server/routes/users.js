98055b2015874c05f353026fcab54d68857a0ba8
Ryan Punwasi <ryan.punwasi@gmail.com>
2023-01-18 16:55:22 -0500

    chore: remove hardcoded user data

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,7 +1,5 @@
 const router = require("express").Router();
 
-const users = ["Bob", "Alex", "Will", "Tristan"];
-
 module.exports = db => {
   router.get("/", (req, res) => {
     db.query("SELECT username FROM users").then(result => {
