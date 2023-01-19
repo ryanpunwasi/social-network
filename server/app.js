e694d1fab868da9a45c49858768ab4b27005a96b
Ryan Punwasi <ryan.punwasi@gmail.com>
2023-01-19 17:19:48 -0500

    chore: removed boilerplate routes

--- server/app.js
+++ server/app.js
@@ -1,10 +1,8 @@
 const express = require("express");
 const path = require("path");
-const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const db = require("./config/db.config");
 
-const indexRouter = require("./routes/index");
 const registerRouter = require("./routes/register");
 const loginRouter = require("./routes/login");
 
@@ -13,10 +11,8 @@ const app = express();
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/i", indexRouter);
 app.use("/register", registerRouter(db));
 app.use("/login", loginRouter(db));
 
