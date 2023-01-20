fd1dbf253ad5829d361912a45cc2397e5c3c9f6d
Ryan Punwasi <ryan.punwasi@gmail.com>
2023-01-19 23:25:58 -0500

    chore: fix typo

--- server/helpers/validation/passwordRules.js
+++ server/helpers/validation/passwordRules.js
@@ -1,6 +1,6 @@
 /**
  * ADD RULES TO THIS FILE
- * Each rule is an object a property called errorMessage and a method called func. The rules are used by validator.js. func is a function that checks the field against some criteria and returns a boolean. errorMessage is the error message to display is func returns false.
+ * Each rule is an object with a property called errorMessage and a method called func. The rules are used by validator.js. func is a function that checks the field against some criteria and returns a boolean. errorMessage is the error message to display is func returns false.
  */
 
 const emptyRule = {
